0ca92e8e696bbabb15a097c38238d852d3202ff6
avadhoot <avadhoot.emails@gmail.com>
2017-05-18 20:39:08 +0530

    css background change

---
+++ routes/index.js
@@ -0,0 +1,79 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+var config = require('../config');
+
+router.get('/', function(req, res) {
+  res.setLocale(config.locale);
+  res.render('index', { community: config.community,
+                        tokenRequired: !!config.inviteToken });
+});
+
+router.post('/invite', function(req, res) {
+  if (req.body.email && (!config.inviteToken || (!!config.inviteToken && req.body.token === config.inviteToken))) {
+    request.post({
+        url: 'https://'+ config.slackUrl + '/api/users.admin.invite',
+        form: {
+          email: req.body.email,
+          token: config.slacktoken,
+          set_active: true
+        }
+      }, function(err, httpResponse, body) {
+        // body looks like:
+        //   {"ok":true}
+        //       or
+        //   {"ok":false,"error":"already_invited"}
+        if (err) { return res.send('Error:' + err); }
+        body = JSON.parse(body);
+        if (body.ok) {
+          res.render('result', {
+            community: config.community,
+            message: '成功！ 檢查 "'+ req.body.email +'" 是否有收到邀請函.'
+          });
+        } else {
+          var error = body.error;
+          if (error === 'already_invited' || error === 'already_in_team') {
+            res.render('result', {
+              community: config.community,
+              message: '您已經被邀請過了<br>' +
+                       '前往 <a href="https://'+ config.slackUrl +'">'+ config.community +'</a>'
+            });
+            return;
+          } else if (error === 'invalid_email') {
+            error = 'e-mail有誤!! 請重新輸入';
+          } else if (error === 'invalid_auth') {
+            error = 'Oops! 有東西出錯了. 請聯絡系統管理員.';
+          }
+
+          res.render('result', {
+            community: config.community,
+            message: '錯誤! ' + error,
+            isFailed: true
+          });
+        }
+      });
+  } else {
+    var errMsg = [];
+    if (!req.body.email) {
+      errMsg.push('請輸入 e-mail');
+    }
+
+    if (!!config.inviteToken) {
+      if (!req.body.token) {
+        errMsg.push('valid token is required');
+      }
+
+      if (req.body.token && req.body.token !== config.inviteToken) {
+        errMsg.push('the token you entered is wrong');
+      }
+    }
+
+    res.render('result', {
+      community: config.community,
+      message: '失敗! ' + errMsg.join(' and ') + '.',
+      isFailed: true
+    });
+  }
+});
+
+module.exports = router;
